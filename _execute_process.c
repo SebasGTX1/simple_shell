6d3bf80c42debe0fda11babca7de10dafbf0b006
SebasGTX1 <sebas-0202@hotmail.es>
2022-04-16 19:07:34 -0500

    error output provisional

--- _execute_process.c
+++ _execute_process.c
@@ -29,7 +29,19 @@ int _execute(char **args)
 		}
 		else
 		{
-			perror("command not found");
+			int i = 0;
+			char i_char, *args_0 = "/hsh", *args_1 = args[0];
+
+			for (; args[i]; i++)
+				;
+			i_char = i + '0';
+			write(STDERR_FILENO, args_0, _strlen(args_0));
+			write(STDERR_FILENO, ": ", 2);
+			write(STDERR_FILENO, &i_char, 1);
+			write(STDERR_FILENO, ": ", 2);
+			write(STDERR_FILENO, args_1, _strlen(args_1));
+			write(STDERR_FILENO, ": ", 2);
+			write(STDERR_FILENO, "not found\n", 10);
 			return (1);
 		}
 	}
--- _process_luncher.c
+++ _process_luncher.c
@@ -12,26 +12,17 @@ int _process_launcher(char **args)
 
 	pid = fork();
 	if (pid == 0)
-	{
-		if (execve(args[0], args, NULL) == -1)
-		{
-			perror("execv fail");
-		}
-		return (1);
-	}
+		execve(args[0], args, NULL);
 	else if (pid < 0)
 	{
-		perror("execv fail 2");
+		return (-1);
 	}
 	else
 	{
 		do {
 			wait_pid = waitpid(pid, &status, WUNTRACED | WCONTINUED);
 			if (wait_pid == -1)
-			{
-				perror("wait error");
 				exit(EXIT_FAILURE);
-			}
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 	return (1);
