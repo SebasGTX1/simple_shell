75e05f99c67ec184fd4a8fc53a610dc0bae66629
SebasGTX1 <sebas-0202@hotmail.es>
2022-04-20 09:38:01 -0500

    fixing error sms in cd buiildin and implementing _getenv

--- build_ins.c
+++ build_ins.c
@@ -5,26 +5,18 @@
  * @args: the argument list
  * Return: always 1
  */
-
 int _cd(char *line __attribute__((unused)), char **args)
 {
 	int error_check, j = 0;
 	char **env = environ, *home, *new_PWD[4];
+	char error[] = "./hsh: 1: cd: can't cd to ", finish = '\n';
 
 	new_PWD[0] = "CD_CALL", new_PWD[1] = "PWD", new_PWD[3] = NULL;
 	if (!args[1])
 	{
-		for (; env[j]; j++)
-		{
-			if ((_strncmp("HOME", env[j], 4)) == 0)
-			{
-				home = _strtok(env[j], "=");
-				home = _strtok(NULL, "=");
-				chdir(home); /*set OLDPWD*/
-				new_PWD[2] = home, _setenv(line, new_PWD);
-				return (1);
-			}
-		}
+		home = _getenv("HOME"), chdir(home); /*set OLDPWD*/
+		new_PWD[2] = home, _setenv(line, new_PWD);
+		return (1);
 	}
 	if ((_strncmp("-", args[1], 1)) == 0)
 	{
@@ -33,8 +25,7 @@ int _cd(char *line __attribute__((unused)), char **args)
 			if ((_strncmp("OLDPWD", env[j], 6)) == 0)
 			{
 				home = _strtok(env[j], "=");
-				home = _strtok(NULL, "=");
-				chdir(home);
+				home = _strtok(NULL, "="), chdir(home);
 				new_PWD[2] = home, _setenv(line, new_PWD);
 				return (1);
 			}
@@ -43,7 +34,10 @@ int _cd(char *line __attribute__((unused)), char **args)
 	chdir(args[1]);
 	new_PWD[2] = args[1], error_check = chdir(args[1]);
 	if (error_check != 0)
-		perror(args[1]);
+	{
+		write(STDOUT_FILENO, error, _strlen(error));
+		write(1, args[1], _strlen(args[1])), write(STDOUT_FILENO, &finish, 1);
+	}
 	else
 		_setenv(line, new_PWD);
 	return (1);
