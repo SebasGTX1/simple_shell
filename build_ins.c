7e369bfc3e86bd7513429be8ae278482d1adbe1c
SebasGTX1 <sebas-0202@hotmail.es>
2022-04-21 18:56:30 -0500

    cd fixing

--- build_ins.c
+++ build_ins.c
@@ -10,17 +10,15 @@
 int _cd(char *line __attribute__((unused)), char **args, int *fail)
 {
 	int error_check;
-	char *home, *new_PWD[4], end = '\n';
+	char *home, end = '\n', cwd[PATH_MAX];
 
 	UNUSED(fail);
-	new_PWD[0] = "CD_CALL", new_PWD[1] = "PWD", new_PWD[3] = NULL;
 	if (!args[1])
 	{
 		home = _getenv("HOME");
 		if (home)
 		{
-			chdir(home);
-			new_PWD[2] = home, _setenv(line, new_PWD, fail);
+			error_check = chdir(home);
 		}
 		return (1);
 	}
@@ -29,21 +27,24 @@ int _cd(char *line __attribute__((unused)), char **args, int *fail)
 		home = _getenv("OLDPWD");
 		if (home)
 		{
-			chdir(home);
-			new_PWD[2] = home, _setenv(line, new_PWD, fail);
+			error_check = chdir(home);
 		}
 		write(STDOUT_FILENO, home, _strlen(home));
 		write(STDOUT_FILENO, &end, 1);
 		return (1);
 	}
-	chdir(args[1]);
-	new_PWD[2] = args[1], error_check = chdir(args[1]);
-	if (error_check != 0)
+	error_check = chdir(args[1]);
+	if (error_check == -1)
 	{
+		perror("hsh");
 		return (1);
 	}
 	else
-		_setenv(line, new_PWD, fail);
+	{
+		getcwd(cwd, sizeof(cwd));
+		setenv("OLDPWD", getenv("PWD"), 1);
+		setenv("PWD", cwd, 1);
+	}
 	return (1);
 }
 /**
